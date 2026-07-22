759e4608bf2231f7db939018db2cd961ca9ada6d
dee565 <deekshitha9595@gmail.com>
2026-07-22 18:10:04 +0530

    Time: 4 ms (94.7%), Space: 32.2 MB (44.03%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,13 +1,6 @@
 class Solution:
-    def isHappy(self, n: int) -> bool:
-        while n!=1:
-            total = 0
-            while n>0:
-                digit=n%10
-                total+=digit*digit
-                n=n//10
-            if total == 4:
-                return False
-            n = total
-        return True
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) != len(set(nums)):
+            return True
+        return False
         
